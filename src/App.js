a17a96b3eb0681cfca334bb8761c5a66d7e4b926
SoniaFaMa <101564312+SoniaFaMa@users.noreply.github.com>
2024-03-12 17:37:57 +0100

    new complement List

--- src/App.js
+++ src/App.js
@@ -1,50 +1,23 @@
 
 import './App.css';
-
-// import Films from './Films.js'
-
-import movies from './movies.js';
-console.log(movies)
-
-
-
-     
+import List from './List'
 
 function App() {
   return (
     <div className="array">
 
       <p>Listado de peliculas</p>
-  
       
-     
-      <ul>
-      {movies.map(item => (
-        <li key={item.title}><b>Title: </b>{item.title}<b>Year: </b>{item.year}<b>Director: </b>{item.director}<b>Duration: </b>{item.duration}<b>Genre: </b>{item.genre}<b>Score: </b>{item.score}</li>
-        
-      ))}
       
-    </ul>
+     <List/>
+      
 
+    
      
     </div>
     
   );
 }
-
-/*function List ({item}){
- 
-  return (
-    
-    <ul>
-      {movies.map(item => (
-        <li key={item.title}><b>Title: </b>{item.title}<b>Year: </b>{item.year}<b>Director: </b>{item.director}<b>Duration: </b>{item.duration}<b>Genre: </b>{item.genre}<b>Score: </b>{item.score}</li>
-        
-      ))}
-      
-    </ul>
-  )
-}*/
 
 
 export default App;
---
+++ src/List.js
@@ -0,0 +1,17 @@
+import movies from './movies.js';
+
+export default function List ({item}){
+ 
+    return (
+      
+      <ul>
+        {movies.map(item => (
+          <li key={item.title}><b>Title: </b>{item.title}<b>Year: </b>{item.year}<b>Director: </b>{item.director}<b>Duration: </b>{item.duration}<b>Genre: </b>{item.genre}<b>Score: </b>{item.score}</li>
+          
+        ))}
+        
+      </ul>
+    )
+  }
+  
+  
