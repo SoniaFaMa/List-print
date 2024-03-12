fbb894f247abb9270104112f2561a396c03ba414
SoniaFaMa <101564312+SoniaFaMa@users.noreply.github.com>
2024-03-12 17:09:24 +0100

    archive movies

---
+++ src/App.js
@@ -0,0 +1,51 @@
+
+import './App.css';
+
+// import Films from './Films.js'
+
+import movies from './movies.js';
+console.log(movies)
+
+
+
+     
+
+function App() {
+  return (
+    <div className="array">
+
+      <p>Listado de peliculas</p>
+      
+      
+      {/* <Films/> */}
+      <ul>
+      {movies.map(item => (
+        <li key={item.title}><b>Title: </b>{item.title}<b>Year: </b>{item.year}<b>Director: </b>{item.director}<b>Duration: </b>{item.duration}<b>Genre: </b>{item.genre}<b>Score: </b>{item.score}</li>
+        
+      ))}
+      
+    </ul>
+
+    
+     
+    </div>
+    
+  );
+}
+
+/*function List ({item}){
+ 
+  return (
+    
+    <ul>
+      {movies.map(item => (
+        <li key={item.title}><b>Title: </b>{item.title}<b>Year: </b>{item.year}<b>Director: </b>{item.director}<b>Duration: </b>{item.duration}<b>Genre: </b>{item.genre}<b>Score: </b>{item.score}</li>
+        
+      ))}
+      
+    </ul>
+  )
+}*/
+
+
+export default App;
